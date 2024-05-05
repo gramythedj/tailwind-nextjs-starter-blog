e21eaa9a9ce11645a8972489a03b74ef4bb8fdf8
Praveen Kumar G <81061885+gramythedj@users.noreply.github.com>
2024-05-05 21:24:41 +0530

    a

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'LOVECULATOR',
+  author: 'Designer Prachi',
+  headerTitle: 'LOVECULATOR.COM',
+  description: 'a Celebrity Fashion blog discussing Clothes, and Jwellery and Makeups',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://loveculator.com',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
